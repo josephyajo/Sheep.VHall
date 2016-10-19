ce09bd5e7f94543610151f2294c7b573edb77f03
josephyajo <joseph@cjzkw.cn>
2016-10-19 22:57:24 +0800

    up

---
+++ Sheep.VHall/Modules/VHallExecption.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Sheep.VHall.Modules
+{
+    [Serializable]
+    public class VHallException : Exception
+    {
+        public VHallException()
+        {
+        }
+
+        public VHallException(string message) : base(message)
+        {
+        }
+
+        protected VHallException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public VHallException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+    }
+}
